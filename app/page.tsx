157f746b3cdda7348c565733ff0e1a84cca2cd00
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-26 11:34:47 -0700

    Add security to application

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,18 @@ import Feed from "../components/Feed";
 import PopularTags from "../components/PopularTags";
 import Search from "../components/Search";
 
-export default function Home() {
+export default async function Home() {
+
   return (
     <section>
       <div className="flex gap-4 justify-center bg-purple-700">
         <div className="container-custom h-50 flex flex-col justify-center p-4 bg-gray-900">
           <h1 className="text-white text-3xl md:text-4xl font-bold mb-2">
-          Developer Solutions Hub
+            Developer Solutions Hub
           </h1>
           <p className="text-white text-sm hidden md:block mb-5">
             A repository of resource for software configuration insights,
-              <br />
+            <br />
             solutions and more.
           </p>
           <p className="text-white text-sm md:hidden mb-5">
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import Link from "next/link";
 import { FaMountainSun } from "react-icons/fa6";
-const Nav = () => {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@app/utils/auth";
+
+const Nav = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <header className="flex justify-center items-center bg-purple-700">
       <nav className="flex container-custom justify-between p-4 bg-gray-900 border-b-1 border-white/20">
-        <Link href={"/"} className="font-semibold text-white flex items-center"><span className="text-purple-600 inline-block mr-1"><FaMountainSun /></span> Chronicles<span className="text-purple-600">Square</span></Link>
-        <div className="flex gap-4 text-white">
-          <Link href={"/profile"} className="text-sm"><i className='bx bx-user' ></i>Profile</Link>
-          <Link href={"/topic/create"} className="text-sm"><i className='bx bx-plus' ></i> Create</Link>
-          <Link href={"/profile/login"} className="text-sm"><i className='bx bx-user-plus'></i> Sign in</Link>
-        </div>
+        <Link href={"/"} className="font-semibold text-white flex items-center">
+          <span className="text-purple-600 inline-block mr-1">
+            <FaMountainSun />
+          </span>{" "}
+          Chronicles<span className="text-purple-600">Square</span>
+        </Link>
+        {session ? (
+          <div className="flex gap-4 text-white">
+            <Link href={"/profile"} className="text-sm">
+              <i className="bx bx-user"></i>Profile
+            </Link>
+            <Link href={"/topic/create"} className="text-sm">
+              <i className="bx bx-plus"></i> Create
+            </Link>
+          </div>
+        ) : (
+          <Link href={"/profile/login"} className="text-sm text-white">
+            <i className="bx bx-user-plus"></i> Sign in
+          </Link>
+        )}
       </nav>
     </header>
   );
